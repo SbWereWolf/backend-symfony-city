7dca3c077c328a56e4cc36bb3e49bb389ed35684
FORGE-JITA\ktokt <ulfnew@gmail.com>
2020-03-22 15:04:44 +0500

    Write file data to Database

--- src/Services/Parser.php
+++ src/Services/Parser.php
@@ -4,24 +4,40 @@ namespace App\Services;
 
 use App\Entity\City;
 use App\Entity\User;
+use App\Repository\CityDatabaseRepository;
 use App\Repository\CityFileRepository;
+use App\Repository\UserDatabaseRepository;
 use App\Repository\UserFileRepository;
 use DomainException;
 use stdClass;
 
 class Parser
 {
-    private $cityRepository;
-    private $userRepository;
+    private $fileForCity;
+    private $fileForUser;
     private $counter = 0;
+    /**
+     * @var CityDatabaseRepository
+     */
+    private CityDatabaseRepository $dbForCity;
+    /**
+     * @var UserDatabaseRepository
+     */
+    private UserDatabaseRepository $dbForUser;
 
-    public function __construct(CityFileRepository $cityRepository, UserFileRepository $userRepository)
+    public function __construct(
+        CityFileRepository $fileForCity,
+        UserFileRepository $fileForUser,
+        CityDatabaseRepository $dbForCity,
+        UserDatabaseRepository $dbForUser)
     {
-        $this->cityRepository = $cityRepository;
-        $this->userRepository = $userRepository;
+        $this->fileForCity = $fileForCity;
+        $this->fileForUser = $fileForUser;
+        $this->dbForCity = $dbForCity;
+        $this->dbForUser = $dbForUser;
     }
 
-    public function __invoke(string $file) : ?int
+    public function __invoke(string $file): ?int
     {
         $oFile = fopen(VAR_DIR . $file, 'r');
         if ($oFile === false) {
@@ -40,7 +56,7 @@ class Parser
         return $this->counter;
     }
 
-    private function processLine(string $line) : bool
+    private function processLine(string $line): bool
     {
         $data = json_decode(trim($line), false, 13, JSON_THROW_ON_ERROR);
         $city = $this->saveData($data);
@@ -55,13 +71,15 @@ class Parser
     private function saveData(stdClass $data): ?City
     {
         $city = new City($data->name, $data->country);
-        if ($this->cityRepository->insert($city) === false) {
+        if ($this->fileForCity->insert($city) === false
+            || $this->dbForCity->insert($city) === false) {
             return null;
         }
 
         foreach ($data->users as $user) {
             $user = new User($user->name, $user->phone);
-            if ($this->userRepository->insert($user, ['city_id' => $city->getId()]) !== false) {
+            if ($this->fileForUser->insert($user, ['city_id' => $city->getId()]) !== false
+                && $this->dbForUser->insert($user, ['city_id' => $city->getId()]) !== false) {
                 $city->attachUser($user);
             }
         }
